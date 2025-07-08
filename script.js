cf30815a75e999f0b6181db750062f0d8c802b84
Jon <126274870+jonmc89@users.noreply.github.com>
2025-07-08 15:43:28 +0100

    Started on JS Script

---
+++ script.js
@@ -0,0 +1,8 @@
+const shareBtn = document.getElementById("shareBtn");
+
+shareBtn.addEventListener("click", () => {
+  console.log("Button Clicked!");
+  const shareContainer = document.getElementsByClassName(
+    "card-profile-container"
+  );
+});
