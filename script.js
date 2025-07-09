0958f6aa328851aefe348d6d959596aaa4f8d259
Jon <126274870+jonmc89@users.noreply.github.com>
2025-07-09 21:27:38 +0100

    Share button functionality

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-const shareBtn = document.getElementById("shareBtn");
+/* Variables */
 
-shareBtn.addEventListener("click", () => {
-  console.log("Button Clicked!");
-  const shareContainer = document.getElementsByClassName(
-    "card-profile-container"
-  );
+const shareIcon = document.getElementById("shareIcon");
+const cardProfileContainer = document.getElementById("cardProfileContainer");
+const originalContent = document.getElementById("cardProfileInfo");
+const cardProfileInfoActive = document.getElementById("cardProfileInfoActive");
+let isActive = false;
+
+shareIcon.addEventListener("click", () => {
+  isActive = !isActive;
+
+  if (isActive) {
+    cardProfileContainer.style.backgroundColor = "hsl(217, 19%, 35%)";
+    originalContent.style.display = "none";
+    cardProfileInfoActive.style.display = "flex";
+  } else {
+    cardProfileContainer.style.backgroundColor = "hsl(210, 46%, 95%)";
+    originalContent.style.display = "flex";
+    cardProfileInfoActive.style.display = "none";
+  }
 });
