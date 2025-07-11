aba7071f1f158aaebec3badb88ba9cccc0f7b08b
Jon <126274870+jonmc89@users.noreply.github.com>
2025-07-11 10:38:24 +0100

    Tablet view finished

--- script.js
+++ script.js
@@ -4,18 +4,27 @@ const shareIcon = document.getElementById("shareIcon");
 const cardProfileContainer = document.getElementById("cardProfileContainer");
 const originalContent = document.getElementById("cardProfileInfo");
 const cardProfileInfoActive = document.getElementById("cardProfileInfoActive");
+const nonMobileProfileActive = document.getElementById(
+  "share-active-icons-non-mob"
+);
 let isActive = false;
 
 shareIcon.addEventListener("click", () => {
   isActive = !isActive;
 
-  if (isActive) {
-    cardProfileContainer.style.backgroundColor = "hsl(217, 19%, 35%)";
-    originalContent.style.display = "none";
-    cardProfileInfoActive.style.display = "flex";
+  if (window.matchMedia("(min-width: 48em)").matches) {
+    // Tablet or larger view
+    nonMobileProfileActive.style.display = isActive ? "block" : "none";
   } else {
-    cardProfileContainer.style.backgroundColor = "hsl(210, 46%, 95%)";
-    originalContent.style.display = "flex";
-    cardProfileInfoActive.style.display = "none";
+    // Mobile View
+    if (isActive) {
+      originalContent.style.display = "none";
+      cardProfileInfoActive.style.display = "flex";
+      cardProfileContainer.style.backgroundColor = "hsl(217, 19%, 35%)";
+    } else {
+      originalContent.style.display = "flex";
+      cardProfileInfoActive.style.display = "none";
+      cardProfileContainer.style.backgroundColor = "hsl(210, 46%, 95%)";
+    }
   }
 });
